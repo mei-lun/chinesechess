5def890563224e9430d2d504e1fd5620f75b733d
meilun <848843249@qq.com>
2023-10-31 15:43:24 +0800

    完成基本的走子功能

--- src/chinesechess.cpp
+++ src/chinesechess.cpp
@@ -15,54 +15,122 @@ chinesechess::~chinesechess()
     delete ui; 
 }
 
+QPixmap GetStdPixmap(QString mPath)
+{
+    QPixmap pixmap(mPath);
+    QBitmap mask = pixmap.createMaskFromColor(QColor(0, 255, 0), Qt::MaskInColor);
+    pixmap.setMask(mask);
+    return pixmap;
+}
+
 void chinesechess::InitEnv()
-{
-    isDrawing = true;
+{ 
+    Piece piece;
+    piece.pixmap = GetStdPixmap(":/images/SELECTED.BMP");
+    piece.status = 0;
+    piece.id = -1;
+    pieceMp.insert(piece.id, piece);
 }
 
-void chinesechess::LevelReverse(int &X, int &Y)
+#define BOARD_ROW 10
+#define BOARD_COL 9
+// 棋盘坐标的水平翻转
+void chinesechess::LevelReverse(qint32 &X, qint32 &Y)
 {
-
+    X = X, Y = BOARD_COL - Y;
 }
 
-void chinesechess::PlumbReverse(int &X, int &Y)
+// 棋盘坐标的垂直翻转
+void chinesechess::PlumbReverse(qint32 &X, qint32 &Y)
 {
-
+    X = BOARD_ROW - X, Y = Y;
 }
 
 void chinesechess::paintEvent(QPaintEvent *e)
 {
-    qDebug()<<isDrawing<<" "<<pieceVt.size();
-    // if(!isDrawing) return;
+    qDebug()<<" "<<pieceMp.count();
     WithDraw();
-    // isDrawing = false;
 }
 
 void chinesechess::WithDraw()
 {
     QPainter painter(this);
-    for(int i = 0; i < pieceVt.size(); i++)
+    for(auto it : pieceMp)
     {
-        QPixmap pixmap = pieceVt[i].pixmap;
-        QBitmap mask = pixmap.createMaskFromColor(QColor(0, 255, 0), Qt::MaskInColor);
-        pixmap.setMask(mask);
-        painter.drawPixmap(pieceVt[i].pos, pixmap);   
+        if(PIECE_IS_LIVE(it.status))
+        {
+            painter.drawPixmap(it.pos, it.pixmap);   
+        }
     }
+}
+
+void chinesechess::SelectPiece(qint32 gx, qint32 gy)
+{
+    qint32 x = G2AX(gy); qint32 y = G2AY(gx);
+    qDebug()<<"tick left "<<x<<" "<<y;
+    if(chess_board[x][y])
+    {
+        Piece* p = &pieceMp[SELECT_MAP_ID];
+        p->pos = QPoint(A2GY(y), A2GX(x));
+        p->status = 1;
+    }
+}
+
+/*
+移动棋子的时候, 如果目标位置没有棋子那么直接赋值
+如果目标位置有棋子那么首先把目标位置的棋子置为死亡,然后再对目标位置赋值
+*/
+void chinesechess::MovePiece(qint32 gx, qint32 gy)
+{
+    qint32 x = G2AX(gy); qint32 y = G2AY(gx);
+    Piece* sp = &pieceMp[SELECT_MAP_ID];
+    auto move2tar = [&](int bx, int by, int ex, int ey, int status = 1){
+        Piece* bp = &pieceMp[G2APOS(bx, by)];
+        bp->id = G2APOS(bx, by);
+        bp->status = status;
+        bp->pos = QPoint(A2GY(ex), A2GX(ey));
+        pieceMp[G2APOS(ex, ey)] = *bp;
+        pieceMp.remove(G2APOS(bx, by));
+        chess_board[ey][ex] = chess_board[by][bx], chess_board[by][bx] = 0;
+    };
+    // 棋盘上的i,j对应棋盘的y, x
+    qint32 ax = x, ay = y;
+    qint32 bx = ay, by = ax;
+    // 目标位置有棋子
+    if(chess_board[x][y])
+    {
+        // 先把目标位置棋子置为死亡
+        move2tar(bx, by, -bx, -by, 2);
+        // 然后把新的棋子移动到这个位置
+        move2tar(G2AX(sp->pos.x()), G2AY(sp->pos.y()), bx, by);
+    }else
+    {
+        // 先把棋子移到目标位置
+        move2tar(G2AX(sp->pos.x()), G2AY(sp->pos.y()), bx, by);
+    }
+    // 消除选择框
+    sp->status = 0;
 }
 
 void chinesechess::mousePressEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton)
-    {
-        isDrawing = true;
+    {   
+        if(!PIECE_IS_LIVE(pieceMp[SELECT_MAP_ID].status))
+        {
+            SelectPiece(event->x(), event->y());
+        }else
+        {
+            qDebug()<< "moveP: x"<< event->x() <<" y:"<< event->y();
+            MovePiece(event->x(), event->y());
+        }
         update(); // 更新画布
-        qDebug()<<"tick left";
         event->accept();
     }
     else
     {   
         qDebug()<<"other event";
-        // update(); // 更新画布
+        // update();
         event->ignore();
     }
 }
@@ -93,9 +161,11 @@ void chinesechess::DrawChessBoard()
                 // 通过棋子的编号得到棋子资源的路径
                 qint32 chessNum = chess_board[i][j];
                 Piece p;
-                p.pixmap = QPixmap(GET_RES(QString(GET_COLOR(chessNum)) + QString(GET_CHESSNAME(chessNum))));
+                p.pixmap = GetStdPixmap(GET_RES(QString(GET_COLOR(chessNum)) + QString(GET_CHESSNAME(chessNum))));
                 p.pos = QPoint(A2GX(j), A2GY(i));
-                pieceVt.push_back(p);
+                p.status = 1;
+                p.id = i * 10 + j;
+                pieceMp.insert(p.id, p);
             }
         }
     }
--- src/chinesechess.h
+++ src/chinesechess.h
@@ -32,11 +32,14 @@
 #define GET_CHESSNAME(PNUM) (chessNum2Name[(qint32)(PNUM % 10)])
 
 // 数组坐标转换成图坐标
-#define A2GX(i) (RL_MARGE + GRRID_SIZE * (j) - PIECE_CENT)
-#define A2GY(i) (UD_MARGE + GRRID_SIZE * (i) - PIECE_CENT)
+#define A2GX(AP) (RL_MARGE + GRRID_SIZE * (AP) - PIECE_CENT)
+#define A2GY(AP) (UD_MARGE + GRRID_SIZE * (AP) - PIECE_CENT)
 // 图坐标转换成数组坐标
 #define G2AX(X) ((qint32)((X - RL_MARGE + PIECE_CENT) / GRRID_SIZE))
 #define G2AY(Y) ((qint32)((Y - UD_MARGE + PIECE_CENT) / GRRID_SIZE))
+
+// 选择图标在MAP中的索引
+#define SELECT_MAP_ID -1
 
 // 资源文件通用前缀
 #define RES_HEAD ":/images/"
@@ -45,13 +48,20 @@
 // 拼接资源文件
 #define GET_RES(FNAME) (QString(RES_HEAD) + QString(FNAME) + QString(RES_LAST))
 
+// 判断棋子的状态
+#define PIECE_IS_LIVE(ST) (ST == 1)
+// 棋盘坐标转换为棋子编号
+#define G2APOS(GX, GY) ((GY) * 10 + (GX))
+// #define A2GPOS(AX, AY) (())
 struct Piece {
-    // 棋子的编号, 和类型id
-    qint32 id = 0, cid = 0;
+    // 棋子的编号
+    qint32 id = 0;
     // 棋子当前的坐标
     QPoint pos;
     // 棋子的像素图
     QPixmap pixmap;
+    // 棋子的状态,0(初始状态),1(就绪状态),2(死亡状态)
+    qint32 status = 0;
 };
 
 class chinesechess : public QMainWindow {
@@ -75,6 +85,8 @@ public:
                                             {0,   0,  0,  0,  0,  0,  0,  0,  0},
                                             {21, 22, 23, 24, 25, 24, 23, 22, 21}};
     QMap<qint32, char> chessNum2Name = {{1, 'R'}, {2, 'N'}, {3, 'B'}, {4, 'A'}, {5, 'K'}, {6, 'C'}, {7, 'P'}};
+    void SelectPiece(qint32 x, qint32 y);
+    void MovePiece(qint32 x, qint32 y);
     void SetWindowStyle();
     void DrawChessBoard();
     void LevelReverse(qint32 &X, qint32 &Y);
@@ -82,6 +94,5 @@ public:
 
 private:
     Ui_chinesechess* ui;
-    bool isDrawing;
-    QVector<Piece> pieceVt;
+    QMap<qint32, Piece> pieceMp;
 };
